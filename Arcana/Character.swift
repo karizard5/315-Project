d2006873d160e72b295d6c45d8dbc090f1299528
Reid, Jon Carl <jreid2@hk223-27731.local>
2018-11-18 21:17:13 -0800

    Core data

---
+++ Arcana/Character.swift
@@ -0,0 +1,23 @@
+//
+//  Character.swift
+//  Arcana
+//
+//  Created by Reid, Jon Carl on 11/18/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import Foundation
+
+
+class Character {
+    
+    var hp: Int
+    var level: Int
+    var strength: Int
+    var name: String
+    var constitution: Int
+    var dexterity: Int
+    var wisdom: Int
+    var intelligence: Int
+    
+}
