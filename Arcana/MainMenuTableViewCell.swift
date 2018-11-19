d3c587a3ac3c08ff81e82fd858336a243689f359
Reid, Jon Carl <jreid2@hk223-27731.local>
2018-11-18 21:09:35 -0800

    main menu table view cell

---
+++ Arcana/MainMenuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainMenuTableViewCell.swift
+//  Arcana
+//
+//  Created by Reid, Jon Carl on 11/18/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
