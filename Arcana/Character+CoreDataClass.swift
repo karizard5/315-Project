47ea43f0d4b0193b77a59e7ce2779a46d822d8c6
Reid, Jon Carl <jreid2@hk223-27731.local>
2018-11-18 21:29:01 -0800

    Good for right now

---
+++ Arcana/Character+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Character+CoreDataClass.swift
+//  Arcana
+//
+//  Created by Reid, Jon Carl on 11/18/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Character)
+public class Character: NSManagedObject {
+
+}
---
+++ Arcana/Character+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Character+CoreDataProperties.swift
+//  Arcana
+//
+//  Created by Reid, Jon Carl on 11/18/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Character {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Character> {
+        return NSFetchRequest<Character>(entityName: "Character")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var level: Int16
+    @NSManaged public var health: Int16
+    @NSManaged public var strength: Int16
+    @NSManaged public var dexterity: Int16
+    @NSManaged public var intelligence: Int16
+    @NSManaged public var constituiton: Int16
+    @NSManaged public var wisdom: Int16
+    @NSManaged public var charisma: Int16
+    @NSManaged public var proficiencies: NSObject?
+
+}
