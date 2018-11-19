fc7223dad70c2bd32b9ede8229b7b3099aa9de23
Reid, Jon Carl <jreid2@hk223-27731.local>
2018-11-18 21:04:05 -0800

    CharacterDetailViewcontroller

---
+++ Arcana/CharacterDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CharacterDetailViewController.swift
+//  Arcana
+//
+//  Created by Reid, Jon Carl on 11/18/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import Foundation
---
+++ CharacterDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterDetailViewController.swift
+//  Arcana
+//
+//  Created by Reid, Jon Carl on 11/18/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
