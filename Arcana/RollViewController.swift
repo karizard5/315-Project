eca228673d9527717140a22999e2739751867916
Reid, Jon Carl <jreid2@hk220-mac2.local>
2018-12-11 19:10:21 -0800

    Roll View Controller

---
+++ Arcana/RollViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RollViewController.swift
+//  Arcana
+//
+//  Created by Reid, Jon Carl on 12/11/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import UIKit
+
+class RollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
