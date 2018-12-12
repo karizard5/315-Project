b3d54b26b9bd01bba2d3a0b798a4450eafd1a77b
Reid, Jon Carl <jreid2@hk220-mac2.local>
2018-12-11 18:51:49 -0800

    Skill change

--- Arcana/AddCharacterViewController.swift
+++ Arcana/AddCharacterViewController.swift
@@ -70,58 +70,83 @@ class AddCharacterViewController: UIViewController, UITextFieldDelegate, UINavig
                 print("Error getting skills")
             }
         } else {
-            var skill = Skill(context: self.context)
-            skill.name = "Strength Save"
-            skill.value = 0
-            skillArray.append(skill)
-            skill.name = "Dexterity Save"
-            skillArray.append(skill)
-            skill.name = "Constitution Save"
-            skillArray.append(skill)
-            skill.name = "Intelligence Save"
-            skillArray.append(skill)
-            skill.name = "Wisdom Save"
-            skillArray.append(skill)
-            skill.name = "Charisma Save"
-            skillArray.append(skill)
-            skill.name = "Acrobatics"
-            skillArray.append(skill)
-            skill.name = "Animal Handling"
-            skillArray.append(skill)
-            skill.name = "Arcana"
-            skillArray.append(skill)
-            skill.name = "Athletics"
-            skillArray.append(skill)
-            skill.name = "Deception"
-            skillArray.append(skill)
-            skill.name = "History"
-            skillArray.append(skill)
-            skill.name = "Insight"
-            skillArray.append(skill)
-            skill.name = "Intimidation"
-            skillArray.append(skill)
-            skill.name = "Intimidation"
-            skillArray.append(skill)
-            skill.name = "Investigation"
-            skillArray.append(skill)
-            skill.name = "Medicine"
-            skillArray.append(skill)
-            skill.name = "Nature"
-            skillArray.append(skill)
-            skill.name = "Perception"
-            skillArray.append(skill)
-            skill.name = "Performance"
-            skillArray.append(skill)
-            skill.name = "Persuasion"
-            skillArray.append(skill)
-            skill.name = "Religion"
-            skillArray.append(skill)
-            skill.name = "Sleight of Hand"
-            skillArray.append(skill)
-            skill.name = "Stealth"
-            skillArray.append(skill)
-            skill.name = "Survival"
-            skillArray.append(skill)
+            let skill1 = Skill(context: self.context)
+            let skill2 = Skill(context: self.context)
+            let skill3 = Skill(context: self.context)
+            let skill4 = Skill(context: self.context)
+            let skill5 = Skill(context: self.context)
+            let skill6 = Skill(context: self.context)
+            let skill7 = Skill(context: self.context)
+            let skill8 = Skill(context: self.context)
+            let skill9 = Skill(context: self.context)
+            let skill10 = Skill(context: self.context)
+            let skill11 = Skill(context: self.context)
+            let skill12 = Skill(context: self.context)
+            let skill13 = Skill(context: self.context)
+            let skill14 = Skill(context: self.context)
+            let skill15 = Skill(context: self.context)
+            let skill16 = Skill(context: self.context)
+            let skill17 = Skill(context: self.context)
+            let skill18 = Skill(context: self.context)
+            let skill19 = Skill(context: self.context)
+            let skill20 = Skill(context: self.context)
+            let skill21 = Skill(context: self.context)
+            let skill22 = Skill(context: self.context)
+            let skill23 = Skill(context: self.context)
+            let skill24 = Skill(context: self.context)
+            let skill25 = Skill(context: self.context)
+            
+            skill1.name = "Strength Save"
+            skill1.value = 0
+            skillArray.append(skill1)
+            skill2.name = "Dexterity Save"
+            skillArray.append(skill2)
+            skill3.name = "Constitution Save"
+            skillArray.append(skill3)
+            skill4.name = "Intelligence Save"
+            skillArray.append(skill4)
+            skill5.name = "Wisdom Save"
+            skillArray.append(skill5)
+            skill6.name = "Charisma Save"
+            skillArray.append(skill6)
+            skill7.name = "Acrobatics"
+            skillArray.append(skill7)
+            skill8.name = "Animal Handling"
+            skillArray.append(skill8)
+            skill9.name = "Arcana"
+            skillArray.append(skill9)
+            skill10.name = "Athletics"
+            skillArray.append(skill10)
+            skill11.name = "Deception"
+            skillArray.append(skill11)
+            skill12.name = "History"
+            skillArray.append(skill12)
+            skill13.name = "Insight"
+            skillArray.append(skill13)
+            skill14.name = "Intimidation"
+            skillArray.append(skill14)
+            skill15.name = "Intimidation"
+            skillArray.append(skill15)
+            skill16.name = "Investigation"
+            skillArray.append(skill16)
+            skill17.name = "Medicine"
+            skillArray.append(skill17)
+            skill18.name = "Nature"
+            skillArray.append(skill18)
+            skill19.name = "Perception"
+            skillArray.append(skill19)
+            skill20.name = "Performance"
+            skillArray.append(skill20)
+            skill21.name = "Persuasion"
+            skillArray.append(skill21)
+            skill22.name = "Religion"
+            skillArray.append(skill22)
+            skill23.name = "Sleight of Hand"
+            skillArray.append(skill23)
+            skill24.name = "Stealth"
+            skillArray.append(skill24)
+            skill25.name = "Survival"
+            skillArray.append(skill25)
         }
     }
 
